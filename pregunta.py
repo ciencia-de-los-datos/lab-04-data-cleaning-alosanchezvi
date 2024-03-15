07d13670ff88258f38175dbfd5d84a3ce5d35a78
alosanchezvi <59671162+alosanchezvi@users.noreply.github.com>
2024-03-15 11:22:40 -0500

    Update pregunta.py

--- pregunta.py
+++ pregunta.py
@@ -14,7 +14,7 @@ def clean_data():
     df = pd.read_csv("solicitudes_credito.csv", sep=";",index_col=0)
     df.reset_index(inplace=True,drop=True)
 
-    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True)
+    df['fecha_de_beneficio'] = pd.to_datetime(df['fecha_de_beneficio'],dayfirst=True, errors='coerce')
 
     df.dropna(axis='index',inplace=True)
     df.drop_duplicates(inplace=True)
